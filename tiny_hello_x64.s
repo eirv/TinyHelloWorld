caa66c98146a3b43ba9c9e395b044dbbb6c9480f
Eirv <2114486405@qq.com>
2025-07-08 23:13:01 +0800

    Make x64 assembly smaller

--- tiny_hello_x64.s
+++ tiny_hello_x64.s
@@ -2,13 +2,12 @@
     .ascii  "\177ELF"           // ELFMAG
 .L.entry:
     leaq    .L.str(%rip), %rsi  // EI_CLASS + EI_DATA + EI_VERSION + EI_OSABI + EI_PAD
-    pushq   $14                 // EI_PAD + 3
-    popq    %rdx                // EI_PAD + 5
+    movb    $1, %dil            // EI_PAD + 3
     jmp     .L.part0            // EI_PAD + 6
     .word   3                   // e_type = ET_DYN
     .word   62                  // e_machine = EM_X86_64
 .L.part0:
-    pushq   $1                  // e_version
+    movb    $1, %al             // e_version
     jmp     .L.part1            // e_version + 2
     .quad   .L.entry - .L.start // e_entry
     .quad   .L.phdr - .L.start  // e_phoff
@@ -25,16 +24,14 @@
     .quad   0                   // p_offset
     .quad   0                   // p_vaddr
 .L.part1:
-    popq    %rax                // p_paddr
-    movq    %rax, %rdi          // p_paddr + 1
-    syscall                     // p_paddr + 4
+    movb    $14, %dl            // p_paddr
+    syscall                     // p_paddr + 2
+    movb    $60, %al            // p_paddr + 4
     jmp     .L.part2            // p_paddr + 6
     .quad   .L.end - .L.start   // p_filesz
     .quad   .L.end - .L.start   // p_memsz
 .L.part2:
-    pushq   $0                  // p_align
-    pushq   $60                 // p_align + 2
-    popq    %rax                // p_align + 4
-    popq    %rdi                // p_align + 5
-    syscall                     // p_align + 6
+    xorl    %edi, %edi          // p_align
+    syscall                     // p_align + 2
+    .zero   4                   // p_align + 4
 .L.end:
